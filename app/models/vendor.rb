8a0d11c92af08226c6c1e7634b9c4bf04b94b41b
Dimitry <dvporg@skyecko.(none)>
2013-05-10 08:29:00 +0000

    some design changes

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,5 +1,5 @@
 class Vendor < ActiveRecord::Base
-  attr_accessible :address, :email, :phone_number, :site_url, :tarif_id, :title
+  attr_accessible :address, :email, :phone_number, :site_url, :tarif_id, :title, :user_id
 
   belongs_to :tarif
   belongs_to :user
